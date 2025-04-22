3c04d2db163a2c024ed7ae2dd9e5aeaf30d24481
ddiske <ddiske@naver.com>
2025-04-22 17:45:30 +0900

    aa

--- src/main/java/com/root/app/user/UserVO.java
+++ src/main/java/com/root/app/user/UserVO.java
@@ -4,8 +4,6 @@ import java.sql.Date;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
