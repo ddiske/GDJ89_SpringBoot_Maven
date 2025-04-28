dd81c001e10455f332c63ce7c70901d6c64cb204
ddiske <ddiske@naver.com>
2025-04-28 09:43:29 +0900

    logoutHandler

--- src/main/java/com/root/app/security/SecurityLogoutHandler.java
+++ src/main/java/com/root/app/security/SecurityLogoutHandler.java
@@ -1,8 +1,11 @@
 package com.root.app.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
+
+import com.root.app.user.UserVO;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,12 +17,17 @@ public class SecurityLogoutHandler implements LogoutHandler {
 
 	@Override
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-		log.info("Before Session {} ", request.getSession()); 
-		log.warn("Before Auth {} ", authentication);
-		request.getSession().invalidate();
-		 log.info("After Session {} ", request.getSession());
-		 log.warn("After Auth {} ", authentication);
-		 log.warn("New Session? {} ", request.getSession().isNew());
+		log.info("Auth : {}", authentication);
+ 		
+ 		//social로그인일 경우 Logout 요청 진행
+ 		if(authentication instanceof OAuth2AuthenticationToken) {
+ 			UserVO userVO =(UserVO)authentication.getPrincipal();
+ 			
+ 			if(userVO.getSns().toUpperCase().equals("KAKAO")) {
+ 				
+ 			}
+ 			
+ 		}
 		
 	}
 
