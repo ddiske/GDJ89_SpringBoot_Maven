758835f3fe17199d81ec9638fb6b23b15b0a0c29
ddiske <ddiske@naver.com>
2025-05-14 17:38:19 +0900

    mui

--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -1,7 +1,10 @@
+import {Button} from "@mui/material"
+
 function Home() {
     return (
         <>
             <h1>Home</h1>
+            <Button variant="contained">Sample</Button>
         </>
     )
 }
